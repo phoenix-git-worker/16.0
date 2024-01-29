1ec6e2fa87d563974921bce0fdbbbaa5fdbcfba8
phoenix-git-worker <eternal.worker.email@gmail.com>
2024-01-29 22:29:27 +0200

    [ADD]dev_hospital: create an installable empty module.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Addons
+# Odoo Dev Practice
+===================
---
+++ dev_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Dev Practice Hospital',
+    'version': '16.0.0.1.0',
+    'summary': 'Hospital',
+    'author': 'phoenix-git-worker',
+    'license': 'LGPL-3',
+    'category': 'Human Resources',
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'auto_install': False,
+    'application': False,
+}
